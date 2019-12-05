15aae38d5cbd6b6632f6284d81151d3684e75958
Gabriel <gabrielhussein83@gmail.com>
2019-12-04 23:45:12 -0300

    Adicionando compressão em tar de forma sequencial

--- trabalho_final.c
+++ trabalho_final.c
@@ -20,9 +20,15 @@ typedef struct info{
 int main(int argc, char**argv){
   int core;
   core = get_nprocs_conf(); //Numero de processadores
+  char bz2append[5] = ".bz2";
+  char oldName[MAX_PATH];
+  strcpy(oldName, argv[1]);
+  strncat(argv[1], bz2append, 4);
+  rename(oldName, argv[1]);
   abrir_diretorio(argv[1]);
-
-
+  char fullpath[MAX_PATH];
+  sprintf(fullpath, "tar -cf %s.tar %s", argv[1], argv[1]);
+  compactar_arquivos(fullpath);
   return 0;
 }
 
@@ -35,11 +41,12 @@ void abrir_diretorio(char*dir_name){
       if(strcmp(entrada->d_name, ".") == 0 || strcmp(entrada->d_name, "..") == 0)continue;
       char path[MAX_PATH];
       snprintf(path, sizeof(path), "%s/%s", dir_name, entrada->d_name);
-      printf("DIR - %s\n", path);
+      //printf("DIR - %s\n", path);
       abrir_diretorio(path);
     }
-    else{
-      printf("File - %s/%s\n", dir_name, entrada->d_name);
+    else
+    {
+      //printf("File - %s/%s\n", dir_name, entrada->d_name);
       char fullpath[MAX_PATH];
       sprintf(fullpath, "bzip2 %s/%s", dir_name, entrada->d_name);
       compactar_arquivos(fullpath);
@@ -50,5 +57,5 @@ void abrir_diretorio(char*dir_name){
 
 void compactar_arquivos(char *file_name){
   FILE * f = popen(file_name, "r");
-  pclose(f);  
+  pclose(f);
 }
