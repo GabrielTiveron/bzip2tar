5e7ffec452e41033d0b476faeb7954149ea68c6c
Gabriel Tiveron <gabrielmtiveron@gmail.com>
2019-12-07 00:47:21 -0300

    Binario

--- trabalho_final.c
+++ trabalho_final.c
@@ -1,47 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<dirent.h>
-#include<string.h>
-#include<bzlib.h>
-#include<sys/sysinfo.h>
+#define _XOPEN_SOURCE 500
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <ftw.h>
 
 #define MAX_PATH 1024
 
-void abrir_diretorio(char*);
-void compactar_arquivos(char*);
-
-typedef struct info{
-  int infd;
-  int outfd;
-  char filename[MAX_PATH];
-}info;
+void abrir_diretorio(char *, char *);
+void compactar_arquivos(char *);
+void delete_dir(char *);
 
 int main(int argc, char**argv){
-  int core;
-  core = get_nprocs_conf(); //Numero de processadores
-  abrir_diretorio(argv[1]);
-
-
+  abrir_diretorio(argv[1], argv[2]);
+  char fullpath[MAX_PATH];
+  sprintf(fullpath, "tar cf %s.bz2.tar %s", argv[1], argv[2]);
+  compactar_arquivos(fullpath);
+  delete_dir(argv[2]);
   return 0;
 }
 
-void abrir_diretorio(char*dir_name){
-  DIR * dir;
-  dir = opendir(dir_name);
+void abrir_diretorio(char* dir_origin, char *dir_dest){
+  dir_dest[strlen(dir_dest)-4] = '\0';
+  mkdir(dir_dest, 0777);
+  DIR *dir = opendir(dir_origin);
   struct dirent *entrada;
   while((entrada = readdir(dir)) != NULL){
     if(entrada->d_type == DT_DIR){
-      if(strcmp(entrada->d_name, ".") == 0 || strcmp(entrada->d_name, "..") == 0)continue;
+      if(strcmp(entrada->d_name, ".") == 0 || strcmp(entrada->d_name, "..") == 0){
+        continue;
+      }
       char path[MAX_PATH];
-      snprintf(path, sizeof(path), "%s/%s", dir_name, entrada->d_name);
-      printf("DIR - %s\n", path);
-      abrir_diretorio(path);
+      snprintf(path, sizeof(path), "%s/%s", dir_origin, entrada->d_name);
+      abrir_diretorio(path, dir_dest);
     }
     else{
-      printf("File - %s/%s\n", dir_name, entrada->d_name);
+      char *forigin = malloc(10000 * sizeof(char));
+      char *fdest = malloc(10000 * sizeof(char));
+      sprintf(forigin, "%s/%s", dir_origin, entrada->d_name);
+      sprintf(fdest, "%s/%s", dir_dest, entrada->d_name);
+      FILE *fp = fopen(forigin, "r");
+      FILE *f = fopen(fdest, "w");
+      char c;
+      while((c = fgetc(fp)) != EOF){
+        fputc(c, f);
+      }
+      fclose(f);
+      fclose(fp);
       char fullpath[MAX_PATH];
-      sprintf(fullpath, "bzip2 %s/%s", dir_name, entrada->d_name);
+      sprintf(fullpath, "bzip2 %s", fdest);
       compactar_arquivos(fullpath);
     }
   }
@@ -49,6 +61,18 @@ void abrir_diretorio(char*dir_name){
 }
 
 void compactar_arquivos(char *file_name){
-  FILE * f = popen(file_name, "r");
-  pclose(f);  
+  FILE *f = popen(file_name, "r");
+  pclose(f);
+}
+
+int delete_files(const char *dir, const struct stat *sb, int typeflag, struct FTW *ftwbuf){
+  int rv = remove(dir);
+  if(rv){
+    perror(dir);
+  }
+  return rv;
+}
+
+void delete_dir(char *dir){
+  nftw(dir, delete_files, 5, FTW_DEPTH);
 }
